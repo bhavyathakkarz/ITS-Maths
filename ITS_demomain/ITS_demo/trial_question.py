4f07259f95bd5798bf5b50480b32b86d48a6c3d5
Bhavya <thakkarbhavya4@gmail.com>
2021-01-23 17:36:08 +0530

    First Commit

---
+++ ITS_demomain/ITS_demo/trial_question.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from fractions import Fraction
+import random
+import os
+
+app = Flask(__name__)
+
+values = [Fraction('8/25'), Fraction('17/4'), Fraction('38/7')]
+Image_folder = os.path.join('static', 'images')
+
+app.config['UPLOAD_FOLDER'] = Image_folder
+full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'cross.jpg')
+
+
+@app.route("/")
+def question():
+    operand = random.choice(values)
+    print(operand)
+    que = 'Express as mixed fraction : '+str(operand)+'.'
+    quo = operand.numerator // operand.denominator
+    rem = operand.numerator % operand.denominator
+    box_ans = [quo, rem, quo, rem, operand.denominator]
+    answer = {'que': que, 'b0': box_ans[0], 'b1': box_ans[1], 'b2': box_ans[2], 'b3': box_ans[3], 'b4': box_ans[4]}
+    return render_template('display.html', answer=answer)
+
+
+app.run(debug=True)
